f7ca2bad7b1aceb1ce8817777da45cebbd422b51
vlada125 <vlada.oleksiuk125@gmail.com>
2023-09-12 15:02:52 +0300

    Configure React app for deployment to page

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,7 @@ const App = () => {
         <BrowserRouter>
           <Provider store={store}>
             <Routes>
+              <Route path="/" element={<HomePage />} />
               <Route path={ROUTES.HOME} element={<HomePage />} />
               <Route path={ROUTES.FILES} element={<Files />} />
               <Route path={ROUTES.LOGIN} element={<LoginPage />} />
